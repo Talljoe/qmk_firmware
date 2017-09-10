a6c59df1846a8b8cede75794a04adf7bb68ec9c2
Joe Wasson <jwasson+github@gmail.com>
2017-09-10 13:15:36 -0700

    MIDI Steno POC

---
+++ layouts/community/ortho_4x12/midi_steno/readme.md
@@ -0,0 +1,3 @@
+# The Default Planck Layout with MIDI for the Plover Layer
+
+ Proof of Concept
---
+++ layouts/community/ortho_4x12/midi_steno/rules.mk
@@ -0,0 +1,5 @@
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+MIDI_ENABLE = yes           # MIDI controls
